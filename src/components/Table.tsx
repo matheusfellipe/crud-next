f8d3c16909fbedd6cc4e93ba34861115e7d2524e
Matheus Fellipe <matheus060399@gmail.com>
2022-07-11 15:09:20 -0400

    add style

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,9 +8,9 @@ export default function Table(props:TableProps){
     function cabecalhoRender(){
         return (
 <tr>
-            <th>Code</th>
-            <th>Name</th>
-            <th>Age</th>
+            <th className='text-left p-4'>Code</th>
+            <th className='text-left p-4'>Name</th>
+            <th className='text-left p-4'>Age</th>
         </tr>
         )
     }
@@ -18,17 +18,17 @@ export default function Table(props:TableProps){
     function dataRender(){
         return props.clientes?.map((cliente,i)=>{
 return (
-    <tr key={cliente.id}>
-        <td>{cliente.id}</td>
-        <td>{cliente.name}</td>
-        <td>{cliente.age}</td>
+    <tr key={cliente.id} className={`${i%2 ===0 ? 'bg-purple-200':'bg-purple-100'}`}>
+        <td className='text-left p-4'>{cliente.id}</td>
+        <td className='text-left p-4'>{cliente.name}</td>
+        <td className='text-left p-4'>{cliente.age}</td>
     </tr>
 )
         })
     }
 return(
-    <table>
-        <thead>
+    <table className='w-full rounded-xl overflow-hidden'>
+        <thead className={`bg-gradient-to-r from-purple-500 to-purple-800`}>
         {cabecalhoRender()}
         </thead>
         <tbody>
